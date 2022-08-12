c8de37cdc5f0a24052d0a957e63c46bd1cd1cc29
neiltarnowski <tarnowski.neil@yahoo.com>
2022-08-12 17:26:18 -0500

    Week 6 BackEnd Assignment

---
+++ backEndWeek6/src/backEndWeek6/Deck.java
@@ -0,0 +1,27 @@
+package backEndWeek6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	
+		List<Card> cards = new ArrayList<Card>();
+		List<String> suits = Arrays.asList("Hearts", "Clubs", "Spades", "Diamonds");
+		
+		public Deck() {
+			for (int i = 2; i < 15; i++) {
+				for(String suit : suits) {
+					cards.add(new Card(i, suit));
+				}
+			}
+		}
+		
+		public void shuffle() {
+			Collections.shuffle(cards);	
+		}
+		public Card draw() {
+			return cards.remove(0);
+		}
+}
